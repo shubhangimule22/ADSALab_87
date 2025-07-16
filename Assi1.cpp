88d86007d67e9c0521f82b98d9cd0589b23e52d9
shubhangimule22 <144653172+shubhangimule22@users.noreply.github.com>
2025-07-16 21:10:01 +0530

    Create Assi1.cpp

---
+++ Assi1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a[10], n, high = 0, low = 0, mid = 0;
+    
+    cout << "\nEnter size of array:\t";
+    cin >> n;
+
+    high = n - 1;
+
+    cout << "\nEnter elements in 1's followed by 0's:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    mid = (high + low) / 2;
+
+    cout << "Count of 0's:\n";
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+
+        if (a[mid] == 1) {
+                      if (mid + 1 < n && a[mid + 1] == 0) {
+                cout << n - (mid + 1) << endl;
+                break;
+            } else {
+                low = mid + 1;
+            }
+        } else if (a[mid] == 0) {
+            if (mid == 0 || a[mid - 1] == 1) {
+                cout << n - mid << endl;
+                break;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return 0;
+}
