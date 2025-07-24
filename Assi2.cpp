c238d1fd762fd33998c4a9155b1fe582fc1a0c37
shubhangimule22 <144653172+shubhangimule22@users.noreply.github.com>
2025-07-24 07:17:54 +0530

    Update Assi2.cpp

--- Assi2.cpp
+++ Assi2.cpp
@@ -1,4 +1,4 @@
 //Title : Implement a program of moving all 0's at end of array.
 //statement : Given an array of numbers push all the 0's at the end of given array.
-//Input : 2 9 0 0 4 0
-//Output : 2 9 4 0 0 0
+//Input : 2 4 0 0 9 0
+//Output : 2 4 9 0 0 0
